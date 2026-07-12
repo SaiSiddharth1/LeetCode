375260b2805cd6464946ef014102c0d81e6ca002
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-07-12 20:46:31 +0530

    Time: 35 ms (24.84%), Space: 46.3 MB (62.84%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -2,15 +2,16 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][][] tabu = new int[n][m][m];
+        int[][] opti = new int[m][m];
         for(int j1 = 0 ; j1 < m ; j1++){
             for(int j2 = 0 ; j2 < m ; j2++){
-                if(j1 == j2) tabu[n-1][j1][j2] = grid[n-1][j1];
-                else tabu[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2] ;
+                if(j1 == j2) opti[j1][j2] = grid[n-1][j1];
+                else opti[j1][j2] = grid[n-1][j1] + grid[n-1][j2] ;
             }
         }
 
         for(int i = n - 2 ; i >= 0 ; i--){
+            int[][] curr = new int[m][m];
             for(int j1 = 0 ; j1 < m ; j1++){
                 for(int j2 = 0 ; j2 < m ; j2++){
                     int max = (int) -1e8;
@@ -20,16 +21,17 @@ class Solution {
                             if(j1 == j2) val = grid[i][j1];
                             else val = grid[i][j1] + grid[i][j2];
                             if(j1 + d1 >= 0 && j2 + d2 >= 0 && j1 + d1 < m && j2 + d2 < m) 
-                                val += tabu[i+1][j1 + d1][j2 + d2];
+                                val += opti[j1 + d1][j2 + d2];
                             else val += (int) -1e8;    
                             max = Math.max(max,val);
                         }
                     }
-                    tabu[i][j1][j2] = max;
+                    curr[j1][j2] = max;
                 }
-            }   
+            }
+            opti = curr;   
         }
 
-        return tabu[0][0][m-1];
+        return opti[0][m-1];
     }
 }
