cdec0a679560baa88f937c8bfc2f29eaec7e70db
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-07-12 20:42:17 +0530

    Time: 38 ms (17%), Space: 46.3 MB (77.32%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -2,31 +2,34 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][][] memo = new int[n][m][m];
-        for(int[][] mat : memo){
-            for(int[] arr : mat){
-                Arrays.fill(arr,-1);
+        int[][][] tabu = new int[n][m][m];
+        for(int j1 = 0 ; j1 < m ; j1++){
+            for(int j2 = 0 ; j2 < m ; j2++){
+                if(j1 == j2) tabu[n-1][j1][j2] = grid[n-1][j1];
+                else tabu[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2] ;
             }
         }
-        return solve(0,0,m-1,n,m,grid,memo);
-    }
-    int solve(int i,int j1 , int j2,int row,int col,int[][] grid,int[][][] memo){
-        if(j1 < 0 || j2 < 0 || j1 > col - 1 || j2 > col - 1) return (int) -1e8;
-        if(i == row - 1){
-            if(j1 == j2){
-                return grid[i][j1]; 
-            }
-            return grid[i][j1] + grid[i][j2]; 
+
+        for(int i = n - 2 ; i >= 0 ; i--){
+            for(int j1 = 0 ; j1 < m ; j1++){
+                for(int j2 = 0 ; j2 < m ; j2++){
+                    int max = (int) -1e8;
+                    for(int d1 = -1 ; d1 <= 1 ; d1++){
+                        for(int d2 = -1 ; d2 <= 1 ; d2++){
+                            int val = 0;
+                            if(j1 == j2) val = grid[i][j1];
+                            else val = grid[i][j1] + grid[i][j2];
+                            if(j1 + d1 >= 0 && j2 + d2 >= 0 && j1 + d1 < m && j2 + d2 < m) 
+                                val += tabu[i+1][j1 + d1][j2 + d2];
+                            else val += (int) -1e8;    
+                            max = Math.max(max,val);
+                        }
+                    }
+                    tabu[i][j1][j2] = max;
+                }
+            }   
         }
-        if(memo[i][j1][j2] != -1) return memo[i][j1][j2];
-        // d is direction
-        int max = 0;
-        for(int d1 = -1 ; d1 <= 1 ; d1++){
-            for(int d2 = -1 ; d2 <= 1 ; d2++){
-                if(j1 == j2) max = Math.max(max,grid[i][j1] + solve(i+1,j1 + d1 ,j2 + d2,row,col,grid,memo));
-                else max = Math.max(max,grid[i][j1] + grid[i][j2] + solve(i+1,j1 + d1 ,j2 + d2,row,col,grid,memo));
-            }
-        }
-        return memo[i][j1][j2] = max;
+
+        return tabu[0][0][m-1];
     }
 }
