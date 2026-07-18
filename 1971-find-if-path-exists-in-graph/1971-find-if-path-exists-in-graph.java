4d0c387be8ab1f7c7403824a7b5c6c00d21ceb2e
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-07-18 12:30:57 +0530

    Time: 18 ms (92.86%), Space: 253.3 MB (91.25%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,30 +1,43 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer,List<Integer>> g = new HashMap<>();
-		for(int i = 0 ; i < n ; i++){
-		    g.put(i,new ArrayList<>());
-		}
+        UFDS ds = new UFDS(n);
         for(int i = 0 ; i < edges.length ; i++){
-		    int u = edges[i][0];
-		    int v = edges[i][1];
-		    g.get(u).add(v);
-		    g.get(v).add(u);
-		}
-        
-        Queue<Integer> q = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-        q.add(source);
-        visited.add(source);
-        while(!q.isEmpty()){
-            int u = q.remove();
-            if(u == destination) return true;
-            for(int v : g.get(u)){
-                if(!visited.contains(v)){
-                    q.add(v);
-                    visited.add(v);
-                }
-            }
+            int u = edges[i][0];
+            int v = edges[i][1];
+            ds.union(u,v);
         }
-        return false;
+        return ds.find(source) == ds.find(destination);
+    }
+}
+
+class UFDS{
+    int islands;
+    int n;
+    int[] parent;
+    public UFDS(int n){
+        this.n = n;
+        this.islands = n;
+        parent = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i;
+        }
+    }
+
+    int find(int x){
+        if(x == parent[x]){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    boolean union(int x,int y){
+        int leaderX = find(x);
+        int leaderY = find(y);
+        if(leaderY == leaderX){
+            return false;
+        }
+        parent[leaderY] = leaderX;
+        islands--;
+        return true;
     }
 }
