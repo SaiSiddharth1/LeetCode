6acf1fed01f07bcaaf10194124d3578593c48d96
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-03 00:12:54 +0530

    Time: 3 ms (86.07%), Space: 43.5 MB (26.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '[' || c == '(' || c == '{'){
+                stk.push(c);
+            }
+            else {
+                if(stk.isEmpty()) return false;
+                else{
+                    char top = stk.pop();
+                    if( c == ']' && top != '[' ||
+                        c == '}' && top != '{' ||
+                        c == ')' && top != '('
+                     ){
+                        return false;
+                     }
+                }
+            }
+        }
+        return stk.isEmpty();
+    }
+}
