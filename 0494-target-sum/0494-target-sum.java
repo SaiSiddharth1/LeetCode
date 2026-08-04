f3f6e5670dff34b95f9e71694038de05c15c3699
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-04 15:41:50 +0530

    Time: 6 ms (72.55%), Space: 45 MB (47.5%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,40 @@
 class Solution {
-    Map<String, Integer> map = new HashMap<>();
+    int offSet;
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums.length - 1,target,nums);
+        // int sum = Arrays.stream(nums).sum();
+        // offSet = sum;
+        // int[][] memo = new int[nums.length][2 * sum + 1];
+        // for(int[] arr : memo){
+        //     Arrays.fill(arr,Integer.MIN_VALUE );
+        // }
+        return countPartitions(nums,target);
     }
-    int solve(int idx,int t,int[] arr){
-        if(idx < 0){
-            return (t == 0) ? 1 : 0;    
+
+    public int countPartitions(int[] arr, int diff) {
+        // code here
+        int n = arr.length;
+        int sum = Arrays.stream(arr).sum();
+        int x = sum - diff;
+        if(x < 0 || x % 2 == 1) return 0;
+        int[][] memo = new int[n][x/2 + 1];
+        for(int[] arrr : memo){
+            Arrays.fill(arrr,-1);
         }
-        String key = idx + "," + t;
-        if(map.containsKey(key)){
-            return map.get(key);
+        return solve(arr.length - 1,x/2,arr,memo);
+    }
+    int mod = (int) 1e9 + 7;
+    int solve(int idx,int target,int[] arr,int[][] memo){
+        if(idx == 0){
+            if(target == 0 && arr[idx] == 0) return 2;
+            else if(target == 0 || arr[idx] == target) return 1;
+            return 0;
         }
-        int m = solve(idx-1,t + arr[idx],arr);
-        int p = solve(idx-1,t - arr[idx],arr);
-        map.put(key,p+m);
-        return  p + m;
+        if(memo[idx][target] != -1) return memo[idx][target];
+        int nT = solve(idx - 1,target,arr,memo);
+        int t = 0;
+        if(arr[idx] <= target){
+            t = solve(idx - 1,target - arr[idx],arr,memo);
+        }
+        return memo[idx][target] = (t + nT) % mod;
     }
 }
