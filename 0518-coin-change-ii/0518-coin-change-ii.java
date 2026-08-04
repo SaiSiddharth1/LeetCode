8bca4a06344b461d4a2d9a7dc066527f0b0b51ad
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-04 22:36:31 +0530

    Time: 11 ms (60.83%), Space: 57.3 MB (22.48%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,26 +1,22 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int[][] tabu = new int[n][amount + 1];
+        for(int i = 0 ; i <= amount ; i++){
+            tabu[0][i] = (i % coins[0] == 0) ? 1 : 0;
         }
-        int ans = min(coins,n - 1 , amount,dp);
-        return ans;
-    }
-    int min(int[] coins,int idx , int t,int[][] dp){
-        if(idx == 0){
-            if(t % coins[0] == 0){
-                return 1;
+
+        for(int idx = 1 ; idx < n ; idx++){
+            for(int t = 0 ; t <= amount ; t++){
+                int notTake = tabu[idx - 1][t];
+                int take = 0;
+                if(coins[idx] <= t){
+                    take = tabu[idx][t - coins[idx]];
+                }
+                tabu[idx][t] = take + notTake;
             }
-            return 0;
         }
-        if(dp[idx][t] != -1) return dp[idx][t];
-        int notTake = min(coins,idx - 1,t,dp);
-        int take = 0;
-        if(coins[idx] <= t){
-            take = min(coins,idx,t - coins[idx],dp);
-        }
-        return dp[idx][t] = take + notTake;
+        int ans = tabu[n - 1][amount];
+        return ans;
     }
 }
