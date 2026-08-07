f3d64e6f707f50318fff78729075f3bd55e12756
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-07 11:21:46 +0530

    Time: 21 ms (54.58%), Space: 54.2 MB (20.67%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,23 +1,25 @@
 class Solution {
     String s1;
     String s2;
-    int[][] memo;
+    int[][] tabu;
     public int longestCommonSubsequence(String text1, String text2) {
         s1 = text1;
         s2 = text2;
-        memo = new int[s1.length()][s2.length()];
-        for(int[] arr : memo){
-            Arrays.fill(arr,-1);
+        int n = s1.length();
+        int m = s2.length();
+        tabu = new int[n + 1][m + 1];
+        
+        for(int i = n - 1 ; i >= 0 ; i--){
+            for(int j = m - 1 ; j >= 0 ; j--){
+                if(s1.charAt(i) == s2.charAt(j)) {
+                    tabu[i][j] = 1 + tabu[i + 1][j+ 1];
+                    continue;
+                }
+                int op1 = tabu[i+1][j];
+                int op2 = tabu[i][j + 1];
+                tabu[i][j] = Math.max(op1,op2);
+            }
         }
-        return LCS(0,0);
-    }
-
-    int LCS(int i,int j){
-        if(i >= s1.length() || j >= s2.length()) return 0;
-        if(s1.charAt(i) == s2.charAt(j)) return 1 + LCS(i + 1 , j+ 1);
-        if(memo[i][j] != -1) return memo[i][j];
-        int op1 = LCS(i+1,j);
-        int op2 = LCS(i,j + 1);
-        return memo[i][j] = Math.max(op1,op2);
+        return tabu[0][0];
     }
 }
