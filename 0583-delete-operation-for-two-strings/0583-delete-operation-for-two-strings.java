c691d6e8939dd8ee1bf86ac83d5192d503f2a1bd
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-10 13:07:41 +0530

    Time: 6 ms (95.25%), Space: 46.4 MB (70.16%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,24 +1,26 @@
 class Solution {
     String s1;
     String s2;
-    int[][] tabu;
+    int[] opti;
     public int minDistance(String word1, String word2) {
         this.s1 = word1;
         this.s2 = word2;
         int n = s1.length();
         int m = s2.length();
-        tabu = new int[n + 1][m + 1];
+        opti = new int[m + 1];
         for(int i = 1 ; i <= n ; i++){
+            int[] temp = new int[m + 1];
             for(int j = 1 ; j <= m ; j++){
                 if(s1.charAt(i - 1) == s2.charAt(j - 1)){
-                    tabu[i][j] = 1 + tabu[i - 1][j - 1];
+                    temp[j] = 1 + opti[j - 1];
                     continue;
                 }
-                int op1 = tabu[i - 1][j];
-                int op2 = tabu[i][j - 1];
-                tabu[i][j] = Math.max(op1,op2);
+                int op1 = opti[j];
+                int op2 = temp[j - 1];
+                temp[j] = Math.max(op1,op2);
             }
+            opti = temp;
         }
-        return (n  + m) - 2 * tabu[n][m];
+        return (n  + m) - 2 * opti[m];
     }
 }
