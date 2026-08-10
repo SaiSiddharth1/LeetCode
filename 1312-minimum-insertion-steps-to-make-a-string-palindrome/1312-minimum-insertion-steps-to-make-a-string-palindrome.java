53840c0d9515682fff52c3516e15e5c3d5d4735e
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-10 12:52:28 +0530

    Time: 17 ms (94.95%), Space: 46.6 MB (85.79%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,21 +1,25 @@
 class Solution {
     String s;
-    int[][] memo;
+    int[][] tabu;
     public int minInsertions(String s) {
         this.s = s;
         int n = s.length();
-        memo = new int[n][n];
-        for(int[] arr : memo) Arrays.fill(arr,-1);
-        return solve(0,n-1);
-    }
-    int solve(int i,int j){
-        if(i >= j) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-        if(s.charAt(i) == s.charAt(j)){
-            return solve(i+1,j-1);
+        tabu = new int[n][n];
+        for(int i = 0 ; i < n ; i++){
+            tabu[i][i] = 0;
         }
-        int op1 = solve(i,j-1);
-        int op2 = solve(i + 1,j);
-        return memo[i][j] = Math.min(op1,op2) + 1;
+        for(int i = n - 2 ; i >= 0 ; i--){
+            for(int j = i + 1 ; j < n ; j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    tabu[i][j] =  tabu[i+1][j-1];
+                    continue;
+                }
+                int op1 = tabu[i][j-1];
+                int op2 = tabu[i + 1][j];
+                tabu[i][j] = Math.min(op1,op2) + 1;
+            }
+        }
+        
+        return tabu[0][n-1];
     }
 }
