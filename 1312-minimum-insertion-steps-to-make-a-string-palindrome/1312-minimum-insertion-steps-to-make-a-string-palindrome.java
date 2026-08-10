06c5c24ec215a5ea02fc982a1febe9648d6f8a96
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-10 12:54:50 +0530

    Time: 15 ms (97.79%), Space: 46.9 MB (73.09%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,25 +1,24 @@
 class Solution {
     String s;
-    int[][] tabu;
+    int[] opti;
     public int minInsertions(String s) {
         this.s = s;
         int n = s.length();
-        tabu = new int[n][n];
-        for(int i = 0 ; i < n ; i++){
-            tabu[i][i] = 0;
-        }
+        opti = new int[n];
         for(int i = n - 2 ; i >= 0 ; i--){
+            int[] temp = new int[n];
             for(int j = i + 1 ; j < n ; j++){
                 if(s.charAt(i) == s.charAt(j)){
-                    tabu[i][j] =  tabu[i+1][j-1];
+                    temp[j] =  opti[j-1];
                     continue;
                 }
-                int op1 = tabu[i][j-1];
-                int op2 = tabu[i + 1][j];
-                tabu[i][j] = Math.min(op1,op2) + 1;
+                int op1 = temp[j-1];
+                int op2 = opti[j];
+                temp[j] = Math.min(op1,op2) + 1;
             }
+            opti = temp;
         }
         
-        return tabu[0][n-1];
+        return opti[n-1];
     }
 }
