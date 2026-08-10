4189b792916e30c8da4dbb5a5ee1a551b61bdacc
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-10 12:12:21 +0530

    Time: 19 ms (85.58%), Space: 46.9 MB (73.09%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -2,22 +2,20 @@ class Solution {
     String s;
     int[][] memo;
     public int minInsertions(String s) {
-       this.s = s;
+        this.s = s;
         int n = s.length();
-        memo = new int[n + 1][n + 1];
-        for(int[] arr : memo){
-            Arrays.fill(arr,-1);
-        }
-        return solve(0,n - 1);
+        memo = new int[n][n];
+        for(int[] arr : memo) Arrays.fill(arr,-1);
+        return solve(0,n-1);
     }
     int solve(int i,int j){
-        if(i >= j ) return 0;
-        if(memo[i][j] != -1) return memo[i][j]; 
+        if(i >= j) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
         if(s.charAt(i) == s.charAt(j)){
-            return memo[i][j] = solve(i + 1,j - 1);
+            return solve(i+1,j-1);
         }
-        int op1 = 1 + solve(i + 1,j);
-        int op2 = 1 + solve(i ,j - 1);
-        return memo[i][j] = Math.min(op1,op2);
+        int op1 = solve(i,j-1);
+        int op2 = solve(i + 1,j);
+        return memo[i][j] = Math.min(op1,op2) + 1;
     }
 }
