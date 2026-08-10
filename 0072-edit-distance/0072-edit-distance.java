9f2676d352f3dba261d4cbe06e5548b49b1b13d3
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-10 13:47:07 +0530

    Time: 5 ms (66.38%), Space: 47.3 MB (35.55%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,31 @@
 class Solution {
     String s1;
     String s2;
-    int[][] memo;
+    int[][] tabu;
     public int minDistance(String word1, String word2) {
         s1 = word1;
         s2 = word2;
         int n = s1.length();
         int m = s2.length();
-        memo = new int[n + 1][m + 1];
-        for(int[] arr : memo) Arrays.fill(arr,-1);
-        return solve(n,m);
-    }
-    int solve(int i,int j){
-        if(i == 0) return j;
-        if(j == 0) return i;
-        if(memo[i][j] != -1) return memo[i][j];
-        if(s1.charAt(i - 1) == s2.charAt(j - 1)){
-            return  memo[i][j] = solve(i - 1 , j - 1);
+        tabu = new int[n + 1][m + 1];
+        for(int i = 0 ; i <= n ; i++){
+            tabu[i][0] = i;
         }
-        int op1 = solve(i - 1,j);
-        int op2 = solve(i,j - 1);
-        int op3 = solve(i - 1,j - 1);
-        return  memo[i][j] = 1 + Math.min(op3,Math.min(op1,op2));
+        for(int j = 0 ; j <= m ; j++){
+            tabu[0][j] = j;
+        }
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    tabu[i][j] = tabu[i - 1][j - 1];
+                    continue;
+                }
+                int op1 = tabu[i-1][j];
+                int op2 = tabu[i][j - 1];
+                int op3 = tabu[i - 1][j - 1];
+                tabu[i][j] = 1 + Math.min(op3,Math.min(op1,op2));
+            }
+        }
+        return tabu[n][m];
     }
 }
