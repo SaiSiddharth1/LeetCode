0266d0f13e94b689182f881edfd22cf80b684bfd
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-11 17:43:13 +0530

    Time: 4 ms (75.61%), Space: 46.3 MB (95.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,32 +1,32 @@
 class Solution {
     String s1;
     String s2;
-    int[][] tabu;
+    int[] opti;
     public int minDistance(String word1, String word2) {
         s1 = word1;
         s2 = word2;
         int n = s1.length();
         int m = s2.length();
-        tabu = new int[n + 1][m + 1];
-        for(int i = 0 ; i <= n ; i++){
-            tabu[i][0] = i;
-        }
-        for(int j = 0 ; j <= m ; j++){
-            tabu[0][j] = j;
+        opti = new int[m + 1];
+        for(int i = 0 ; i <= m ; i++){
+            opti[i] = i;
         }
 
         for(int i = 1 ; i <= n ; i++){
+            int[] temp = new int[m + 1];
+            temp[0] = i;
             for(int j = 1 ; j <= m ; j++){
                 if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    tabu[i][j] = tabu[i-1][j-1];
+                    temp[j] = opti[j-1];
                     continue;
                 }
-                int op1 = tabu[i][j-1];
-                int op2 = tabu[i-1][j];
-                int op3 = tabu[i-1][j-1];
-                tabu[i][j] = 1 + Math.min(op3,Math.min(op1,op2));
+                int op1 = temp[j-1];
+                int op2 = opti[j];
+                int op3 = opti[j-1];
+                temp[j] = 1 + Math.min(op3,Math.min(op1,op2));
             }
+            opti = temp;
         }
-        return tabu[n][m];
+        return opti[m];
     }
 }
