9cf4c3010148eed15605dec4a6f6d40e03a3eaf4
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-11 15:38:51 +0530

    Time: 19 ms (68.55%), Space: 54.4 MB (52.67%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,24 +1,24 @@
 class Solution {
     String s,t;
-    int[][] memo;
+    int[][] tabu;
     public int numDistinct(String s, String t) {
         this.s = s;
         this.t = t;
         int n = s.length();
         int m = t.length();
-        memo = new int[n + 1][m + 1];
-        for(int[] arr : memo){
-            Arrays.fill(arr,-1);
+        tabu = new int[n + 1][m + 1];
+        for(int i = 0 ; i <= n ; i++){
+            tabu[i][0] = 1;
         }
-        return solve(n,m);
-    }
-    int solve(int i,int j){
-        if(j == 0) return 1;
-        if(i == 0) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-        if(s.charAt(i - 1) == t.charAt(j - 1)){
-            return memo[i][j] = solve(i-1,j-1) + solve(i-1,j); 
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    tabu[i][j] = tabu[i-1][j-1] + tabu[i-1][j]; 
+                    continue;
+                }
+                tabu[i][j] = tabu[i-1][j];
+            }
         }
-        return memo[i][j] = solve(i-1,j);
+        return tabu[n][m];
     }
 }
