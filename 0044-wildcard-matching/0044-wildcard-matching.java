f48000606ac491ea9aa6d79df198a2edd6d58675
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-13 13:45:48 +0530

    Time: 25 ms (59.98%), Space: 63.1 MB (18.82%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -6,18 +6,18 @@ class Solution {
         this.p = p;
         int n = s.length();
         int m = p.length();
-        memo = new int[n][m];
+        memo = new int[n + 1][m + 1];
         for(int[] arr : memo){
         Arrays.fill(arr,-1);
         }
-        return solve(n - 1,m - 1);
+        return solve(n,m);
     }
 
     boolean solve(int i,int j){
-        if(i < 0 && j < 0) return true;
-        if(j < 0 && i >= 0) return false;
-        if(i < 0 && j >= 0){
-            if(p.charAt(j) == '*'){
+        if(i == 0 && j == 0) return true;
+        if(j == 0 && i > 0) return false;
+        if(i == 0 && j > 0){
+            if(p.charAt(j - 1) == '*'){
                 return solve(i,j-1);
             }
             return false;
@@ -26,7 +26,7 @@ class Solution {
         if(memo[i][j] != -1){
             return (memo[i][j] == 1) ? true : false;
         }
-        if(p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)){
+        if(p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)){
             boolean anss = solve(i-1,j-1);
             memo[i][j] = (anss) ? 1 : 0;
             return  anss;
@@ -34,7 +34,7 @@ class Solution {
         boolean op1 = false;
         boolean op2 = false;
         boolean op3 = false;
-        if(p.charAt(j) == '*'){
+        if(p.charAt(j - 1) == '*'){
             op1 = solve(i,j-1);
             op2 = solve(i-1,j);
             op3 = solve(i-1,j - 1);
