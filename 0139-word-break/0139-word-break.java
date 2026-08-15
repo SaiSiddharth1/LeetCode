b3571deaf31d67a9307b0b1774f1d54ffb063910
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-15 12:12:21 +0530

    Time: 10 ms (11.03%), Space: 46.2 MB (35.5%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,28 +1,26 @@
 class Solution {
     String s;
+    boolean[] tabu;
     Set<String> set;
-    Boolean[] memo;
     public boolean wordBreak(String s, List<String> wordDict) {
         this.s = s;
-        memo = new Boolean[s.length()];
+        int n = s.length();
+        tabu = new boolean[n + 1];
         set = new HashSet<>();
         for(String ss : wordDict){
             set.add(ss);
         }
-        return solve(s.length() - 1);
-    }
-
-    boolean solve(int idx){
-        if(idx < 0) return true;
-        if(memo[idx] != null) return memo[idx];
-        for(int start = 0 ; start <= idx ; start++){
+        for(int idx = 0 ; idx < n ; idx++){
+            for(int start = 0 ; start <= idx ; start++){
             String word = s.substring(start,idx + 1);
-            if(set.contains(word)){
-                if(solve(start - 1)){
-                    return memo[idx] = true;
+                if(set.contains(word)){
+                    if(start == 0 || tabu[start - 1]){
+                        tabu[idx] = true;
+                        break;
+                    }
                 }
-            }
+            }   
         }
-        return memo[idx] = false;
+        return tabu[n - 1];
     }
 }
