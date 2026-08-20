99939efa75de08f46be80f0e1e6a7cc93b14359c
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-20 13:06:53 +0530

    Time: 29 ms (5.05%), Space: 58.1 MB (65.76%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -7,47 +7,41 @@ class Solution {
         n = grid.length;
         m = grid[0].length;
         visited = new int[n][m];
-
-        for(int j = 0 ; j < m  ; j++){
-            if(grid[0][j] == 1){
-                dfs(0,j,grid);
-            }
-            if(grid[n-1][j] == 1){
-                dfs(n-1,j,grid);
-            }
-        }
-        for(int i = 1 ; i < n-1  ; i++){
-            if(grid[i][0] == 1){
-                dfs(i,0,grid);
-            }
-            if(grid[i][m-1] == 1){
-                dfs(i,m-1,grid);
-            }
-        }
-        int sum = 0;
-        for(int[] arr : grid){
-            for(int i : arr){
-                sum += i;
-            }
-        }
-        System.out.println(Arrays.deepToString(grid));
-        return sum;
-    }
-
-    void dfs(int x,int y,int[][] grid){
-        visited[x][y] = 1;
-        grid[x][y] = 0;
-        for(int d = 0 ; d < 4 ; d++){
-            int newX = x + dx[d];
-            int newY = y + dy[d];
-            if(check(newX,newY) && visited[newX][newY] == 0){
-                if(grid[newX][newY] == 1){
-                    dfs(newX,newY,grid);
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(i == 0 || j == 0 || i == n - 1 || j == m - 1){
+                    if(grid[i][j] == 1){ 
+                        visited[i][j] = 1;
+                        q.add(new int[]{i,j});
+                    }
                 }
             }
         }
+        while(!q.isEmpty()){
+            int[] curr = q.remove();
+            int x = curr[0];
+            int y = curr[1];
+            for(int d = 0 ; d < 4 ; d++){
+                int newX = x + dx[d];
+                int newY = y + dy[d];
+                if(check(newX,newY)&& visited[newX][newY] == 0){
+                    if(grid[newX][newY] == 1){
+                        visited[newX][newY] = 1;
+                        q.add(new int[]{newX,newY});
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(visited));
+        int c = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1 && visited[i][j] == 0) c++;
+            }
+        }
+        return c;
     }
-
     boolean check(int x,int y){
         return (x >=0 && y >= 0 && x < n && y < m);
     }
