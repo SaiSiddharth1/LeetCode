be044041d2583630fd46ce68b6d8565f70b351b4
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-08-24 23:34:25 +0530

    Time: 30 ms (14.75%), Space: 60.4 MB (83.97%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,49 +1,37 @@
 class Solution {
-    int[] visited;
-    int[] pathVisited;
-    int[] check;
-    List<List<Integer>> adj;
+    int[] inDegree;
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<List<Integer>> adj = new ArrayList<>();
         int n = graph.length;
-        adj = new ArrayList<>();
         for(int i = 0 ; i < n ; i++){
             adj.add(new ArrayList<>());
+        }
+        inDegree = new int[n];
+        for(int i = 0 ; i < n ; i++){
             for(int x : graph[i]){
-                adj.get(i).add(x);
+                adj.get(x).add(i);
+                inDegree[i]++;
             }
         }
-        List<Integer> safe;
-        visited = new int[n];
-        pathVisited = new int[n];
-        check = new int[n];
-        safe = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> safe = new ArrayList<>();
         for(int i = 0 ; i < n ; i++){
-            if(visited[i] == 0){
-                dfs(i);
-            }
-        }
-        for(int i = 0 ; i < n ; i++){
-            if(check[i] == 1){
+            if(inDegree[i] == 0){
+                q.add(i);
                 safe.add(i);
             }
         }
-        // Collections.reverse(order);
-        return safe;
-    }
-
-    boolean dfs(int i){
-        visited[i] = 1;
-        pathVisited[i] = 1;
-        for(int x : adj.get(i)){
-            if(visited[x]==1 && pathVisited[x] == 1){
-                return true;
-            }
-            if(visited[x]==0){
-                if(dfs(x)) return true;
+        while(!q.isEmpty()){
+            int node = q.remove();
+            for(int x : adj.get(node)){
+                inDegree[x]--;
+                if(inDegree[x] == 0){
+                    q.add(x);
+                    safe.add(x);    
+                }
             }
         }
-        pathVisited[i] = 0;
-        check[i] = 1;
-        return false;
+        Collections.sort(safe);
+        return safe;
     }
 }
