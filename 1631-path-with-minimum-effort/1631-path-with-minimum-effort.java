65e1e1bbe15b7067c76fcf31b99fbd195c6ef4fa
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-09-04 18:40:40 +0530

    Time: 65 ms (17.42%), Space: 47.8 MB (75.88%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+        int[][] diff = new int[n][m];
+        for(int[] arr : diff){
+            Arrays.fill(arr,(int) 1e9);
+        }
+        diff[0][0] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            if(a[0] != b[0]){
+                return Integer.compare(a[0],b[0]);
+            }
+            return Integer.compare(a[1],b[1]);
+        });
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        pq.add(new int[]{0,0,0});
+        while(!pq.isEmpty()){
+            int[] curr = pq.remove();
+            int dist = curr[0];
+            int x = curr[1];
+            int y = curr[2];
+            for(int d = 0 ; d < 4 ; d++){
+                int newX = dx[d] + x;
+                int newY = dy[d] + y;
+                if(check(newX,newY,n,m)){
+                    int h1 = heights[newX][newY];
+                    int h2 = heights[x][y];
+                    int newEffort = Math.max(Math.abs(h1-h2),dist);
+                    if(newEffort < diff[newX][newY]){
+                        diff[newX][newY] = newEffort;
+                        pq.add(new int[]{newEffort,newX,newY});
+                    }   
+                }
+            }
+        }
+        return diff[n-1][m-1];
+    }
+
+    boolean check(int x,int y,int n,int m){
+        return(x >= 0 && x < n && y >= 0 && y < m);
+    }
+}
