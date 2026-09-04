49ac9a78b3ebf80882f9301bce7cab03f5bed42a
SaiSiddharth1 <161485075+SaiSiddharth1@users.noreply.github.com>
2026-09-04 18:47:26 +0530

    Time: 81 ms (11.09%), Space: 47.9 MB (59.74%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -7,17 +7,20 @@ class Solution {
             Arrays.fill(arr,(int) 1e9);
         }
         diff[0][0] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+        TreeSet<int[]> pq = new TreeSet<>((a,b)->{
             if(a[0] != b[0]){
                 return Integer.compare(a[0],b[0]);
             }
-            return Integer.compare(a[1],b[1]);
+            if(a[1] != b[1]){
+                return Integer.compare(a[1],b[1]);
+            }
+            return Integer.compare(a[2],b[2]);
         });
         int[] dx = {-1,0,1,0};
         int[] dy = {0,1,0,-1};
         pq.add(new int[]{0,0,0});
         while(!pq.isEmpty()){
-            int[] curr = pq.remove();
+            int[] curr = pq.pollFirst();
             int dist = curr[0];
             int x = curr[1];
             int y = curr[2];
@@ -29,6 +32,9 @@ class Solution {
                     int h2 = heights[x][y];
                     int newEffort = Math.max(Math.abs(h1-h2),dist);
                     if(newEffort < diff[newX][newY]){
+                        if(diff[newX][newY] == (int) 1e9){
+                            pq.remove(new int[]{diff[newX][newY],newX,newY});
+                        }
                         diff[newX][newY] = newEffort;
                         pq.add(new int[]{newEffort,newX,newY});
                     }   
